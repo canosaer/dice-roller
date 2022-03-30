ecd5a9a37c8abe24d8147c17bc10955e11cc0f46
canosaer <canosaer@gmail.com>
2022-03-30 14:44:44 -0400

    fixed

--- src/js/main.js
+++ src/js/main.js
@@ -58,38 +58,22 @@ let rollDie = function(dieCount){
 }
 
 socket.on('rolled', function (data) {
-	setTimeout(function(){
-		dieResult = 0
-		for(let i=0;i<data.rolls.length;i++){
-			dieResult += data.rolls[i]
-		}
-	}, 300);
+	dieResult = 0
+	for(let i=0;i<data.rolls.length;i++){
+		dieResult += data.rolls[i]
+	}
 	resetLog()
-<<<<<<< HEAD
 
 	for(let i=0;i<logPS.length-1;i++){
 		logPS[i].textContent = logPS[i+1].textContent
 	}
-<<<<<<< HEAD
 	logPS[logPS.length-1].textContent = `${data.name} rolled ${data.count}d${data.faces}${addSub}${addSubValue}: ${dieResult}`
 
-=======
-	setTimeout(function(){
-		for(let i=0;i<logPS.length-1;i++){
-			logPS[i].textContent = logPS[i+1].textContent
-		}
-		logPS[logPS.length-1].textContent = `${data.name} rolled ${data.count}d${data.faces}${addSub}${addSubValue}: ${dieResult}`
-	}, 400);
->>>>>>> parent of bbae2fc (1.1)
 });
 
 let resetLog = function(){
 	addSub = ``
 	addSubValue = ``
-=======
-	logPS[logPS.length-1].textContent = `${name} rolled ${dieNum}d${dieFace}${addSub}${addSubValue}: ${roll}`
-
->>>>>>> parent of 6e0e632 (adjustments)
 }
 
 beginBUTTON.addEventListener(`click`, function(){
@@ -97,7 +81,7 @@ beginBUTTON.addEventListener(`click`, function(){
 	displayDIV.style.border = `2vh black double`
 	rollerSECTION.style.display = `flex`
 	if(nameINPUT.value === ``){
-		name = `unknown`
+		name = `anon`
 	}
 	else name = nameINPUT.value
 })
@@ -108,57 +92,57 @@ for(let i=0; i<dieFIGURES.length; i++){
 			dieFace = 4
 			dieNum = 1
 			rollDie(dieNum)
-			setTimeout(function(){
-				resultP.textContent = dieResult
-			}, 350);
+			
+			resultP.textContent = dieResult
+			
 		}
 		else if(i===1){
 			dieFace = 6
 			dieNum = 1
 			rollDie(dieNum)
-			setTimeout(function(){
-				resultP.textContent = dieResult
-			}, 350);
+			
+			resultP.textContent = dieResult
+			
 		}
 		else if(i===2){
 			dieFace = 8
 			dieNum = 1
 			rollDie(dieNum)
-			setTimeout(function(){
-				resultP.textContent = dieResult
-			}, 350);
+			
+			resultP.textContent = dieResult
+			
 		}
 		else if(i===3){
 			dieFace = 10
 			dieNum = 1
 			rollDie(dieNum)
-			setTimeout(function(){
-				resultP.textContent = dieResult
-			}, 350);
+			
+			resultP.textContent = dieResult
+			
 		}
 		else if(i===4){
 			dieFace = 12
 			dieNum = 1
 			rollDie(dieNum)
-			setTimeout(function(){
-				resultP.textContent = dieResult
-			}, 350);
+			
+			resultP.textContent = dieResult
+			
 		}
 		else if(i===5){
 			dieFace = 20
 			dieNum = 1
 			rollDie(dieNum)
-			setTimeout(function(){
-				resultP.textContent = dieResult
-			}, 350);
+			
+			resultP.textContent = dieResult
+			
 		}
 		else{
 			dieFace = 100
 			dieNum = 1
 			rollDie(dieNum)
-			setTimeout(function(){
-				resultP.textContent = dieResult
-			}, 350);
+			
+			resultP.textContent = dieResult
+			
 		}
 	})
 }
@@ -176,19 +160,19 @@ for(let i=0; i<rollBUTTONS.length; i++){
 
 			rollDie(dieNum)
 			let radioINPUTS = document.querySelectorAll(`.selection input[type=radio]`)
-			setTimeout(function(){
-				if(radioINPUTS[0].checked === true && parseInt(numberINPUTS[1].value) > 0){
-					addSub = `+`
-					addSubValue = numberINPUTS[1].value
-					dieResult = dieResult + parseInt(numberINPUTS[1].value)
-				}
-				else if(radioINPUTS[1].checked === true && parseInt(numberINPUTS[1].value) > 0){
-					addSub = `-`
-					addSubValue = numberINPUTS[1].value
-					dieResult = dieResult - parseInt(numberINPUTS[1].value)
-				}
-				resultP.textContent = dieResult
-			}, 350);
+			
+			if(radioINPUTS[0].checked === true && parseInt(numberINPUTS[1].value) > 0){
+				addSub = `+`
+				addSubValue = numberINPUTS[1].value
+				dieResult = dieResult + parseInt(numberINPUTS[1].value)
+			}
+			else if(radioINPUTS[1].checked === true && parseInt(numberINPUTS[1].value) > 0){
+				addSub = `-`
+				addSubValue = numberINPUTS[1].value
+				dieResult = dieResult - parseInt(numberINPUTS[1].value)
+			}
+			resultP.textContent = dieResult
+			
 
 			
 			
@@ -204,19 +188,19 @@ for(let i=0; i<rollBUTTONS.length; i++){
 			rollDie(dieNum)
 			let radioINPUTS = document.querySelectorAll(`.selection input[type=radio]`)
 
-			setTimeout(function(){
-				if(radioINPUTS[2].checked === true && parseInt(numberINPUTS[3].value) > 0){
-					addSub = `+`
-					addSubValue = numberINPUTS[3].value
-					dieResult = dieResult + parseInt(numberINPUTS[3].value)
-				}
-				else if(radioINPUTS[3].checked === true && parseInt(numberINPUTS[3].value) > 0){
-					addSub = `-`
-					addSubValue = numberINPUTS[3].value
-					dieResult = dieResult - parseInt(numberINPUTS[3].value)
-				}
-				resultP.textContent = dieResult
-			}, 350);
+		
+			if(radioINPUTS[2].checked === true && parseInt(numberINPUTS[3].value) > 0){
+				addSub = `+`
+				addSubValue = numberINPUTS[3].value
+				dieResult = dieResult + parseInt(numberINPUTS[3].value)
+			}
+			else if(radioINPUTS[3].checked === true && parseInt(numberINPUTS[3].value) > 0){
+				addSub = `-`
+				addSubValue = numberINPUTS[3].value
+				dieResult = dieResult - parseInt(numberINPUTS[3].value)
+			}
+			resultP.textContent = dieResult
+			
 
 			
 		}
@@ -231,19 +215,19 @@ for(let i=0; i<rollBUTTONS.length; i++){
 			rollDie(dieNum)
 			let radioINPUTS = document.querySelectorAll(`.selection input[type=radio]`)
 
-			setTimeout(function(){
-				if(radioINPUTS[4].checked === true && parseInt(numberINPUTS[5].value) > 0){
-					addSub = `+`
-					addSubValue = numberINPUTS[5].value
-					dieResult = dieResult + parseInt(numberINPUTS[5].value)
-				}
-				else if(radioINPUTS[5].checked === true && parseInt(numberINPUTS[5].value) > 0){
-					addSub = `-`
-					addSubValue = numberINPUTS[5].value
-					dieResult = dieResult - parseInt(numberINPUTS[5].value)
-				}
-				resultP.textContent = dieResult
-			}, 350);
+			
+			if(radioINPUTS[4].checked === true && parseInt(numberINPUTS[5].value) > 0){
+				addSub = `+`
+				addSubValue = numberINPUTS[5].value
+				dieResult = dieResult + parseInt(numberINPUTS[5].value)
+			}
+			else if(radioINPUTS[5].checked === true && parseInt(numberINPUTS[5].value) > 0){
+				addSub = `-`
+				addSubValue = numberINPUTS[5].value
+				dieResult = dieResult - parseInt(numberINPUTS[5].value)
+			}
+			resultP.textContent = dieResult
+			
 
 
 			
@@ -259,19 +243,19 @@ for(let i=0; i<rollBUTTONS.length; i++){
 			rollDie(dieNum)
 			let radioINPUTS = document.querySelectorAll(`.selection input[type=radio]`)
 
-			setTimeout(function(){
-				if(radioINPUTS[6].checked === true && parseInt(numberINPUTS[7].value) > 0){
-					addSub = `+`
-					addSubValue = numberINPUTS[7].value
-					dieResult = dieResult + parseInt(numberINPUTS[7].value)
-				}
-				else if(radioINPUTS[7].checked === true && parseInt(numberINPUTS[7].value) > 0){
-					addSub = `-`
-					addSubValue = numberINPUTS[7].value
-					dieResult = dieResult - parseInt(numberINPUTS[7].value)
-				}
-				resultP.textContent = dieResult
-			}, 350);
+		
+			if(radioINPUTS[6].checked === true && parseInt(numberINPUTS[7].value) > 0){
+				addSub = `+`
+				addSubValue = numberINPUTS[7].value
+				dieResult = dieResult + parseInt(numberINPUTS[7].value)
+			}
+			else if(radioINPUTS[7].checked === true && parseInt(numberINPUTS[7].value) > 0){
+				addSub = `-`
+				addSubValue = numberINPUTS[7].value
+				dieResult = dieResult - parseInt(numberINPUTS[7].value)
+			}
+			resultP.textContent = dieResult
+			
 			
 		}
 		else if(i===4){
@@ -285,19 +269,19 @@ for(let i=0; i<rollBUTTONS.length; i++){
 			rollDie(dieNum)
 			let radioINPUTS = document.querySelectorAll(`.selection input[type=radio]`)
 
-			setTimeout(function(){
-				if(radioINPUTS[8].checked === true && parseInt(numberINPUTS[9].value) > 0){
-					addSub = `+`
-					addSubValue = numberINPUTS[9].value
-					dieResult = dieResult + parseInt(numberINPUTS[9].value)
-				}
-				else if(radioINPUTS[9].checked === true && parseInt(numberINPUTS[9].value) > 0){
-					addSub = `-`
-					addSubValue = numberINPUTS[9].value
-					dieResult = dieResult - parseInt(numberINPUTS[9].value)
-				}
-				resultP.textContent = dieResult
-			}, 350);
+			
+			if(radioINPUTS[8].checked === true && parseInt(numberINPUTS[9].value) > 0){
+				addSub = `+`
+				addSubValue = numberINPUTS[9].value
+				dieResult = dieResult + parseInt(numberINPUTS[9].value)
+			}
+			else if(radioINPUTS[9].checked === true && parseInt(numberINPUTS[9].value) > 0){
+				addSub = `-`
+				addSubValue = numberINPUTS[9].value
+				dieResult = dieResult - parseInt(numberINPUTS[9].value)
+			}
+			resultP.textContent = dieResult
+			
 
 			
 		}
@@ -312,19 +296,19 @@ for(let i=0; i<rollBUTTONS.length; i++){
 			rollDie(dieNum)
 			let radioINPUTS = document.querySelectorAll(`.selection input[type=radio]`)
 
-			setTimeout(function(){
-				if(radioINPUTS[10].checked === true && parseInt(numberINPUTS[11].value) > 0){
-					addSub = `+`
-					addSubValue = numberINPUTS[11].value
-					dieResult = dieResult + parseInt(numberINPUTS[11].value)
-				}
-				else if(radioINPUTS[11].checked === true && parseInt(numberINPUTS[11].value) > 0){
-					addSub = `-`
-					addSubValue = numberINPUTS[11].value
-					dieResult = dieResult - parseInt(numberINPUTS[11].value)
-				}
-				resultP.textContent = dieResult
-			}, 350);
+			
+			if(radioINPUTS[10].checked === true && parseInt(numberINPUTS[11].value) > 0){
+				addSub = `+`
+				addSubValue = numberINPUTS[11].value
+				dieResult = dieResult + parseInt(numberINPUTS[11].value)
+			}
+			else if(radioINPUTS[11].checked === true && parseInt(numberINPUTS[11].value) > 0){
+				addSub = `-`
+				addSubValue = numberINPUTS[11].value
+				dieResult = dieResult - parseInt(numberINPUTS[11].value)
+			}
+			resultP.textContent = dieResult
+		
 	
 		}
 		else if(i===6){
@@ -338,19 +322,19 @@ for(let i=0; i<rollBUTTONS.length; i++){
 			rollDie(dieNum)
 			let radioINPUTS = document.querySelectorAll(`.selection input[type=radio]`)
 
-			setTimeout(function(){
-				if(radioINPUTS[12].checked === true && parseInt(numberINPUTS[13].value) > 0){
-					addSub = `+`
-					addSubValue = numberINPUTS[13].value
-					dieResult = dieResult + parseInt(numberINPUTS[13].value)
-				}
-				else if(radioINPUTS[13].checked === true && parseInt(numberINPUTS[13].value) > 0){
-					addSub = `-`
-					addSubValue = numberINPUTS[13].value
-					dieResult = dieResult - parseInt(numberINPUTS[13].value)
-				}
-				resultP.textContent = dieResult
-			}, 350);
+		
+			if(radioINPUTS[12].checked === true && parseInt(numberINPUTS[13].value) > 0){
+				addSub = `+`
+				addSubValue = numberINPUTS[13].value
+				dieResult = dieResult + parseInt(numberINPUTS[13].value)
+			}
+			else if(radioINPUTS[13].checked === true && parseInt(numberINPUTS[13].value) > 0){
+				addSub = `-`
+				addSubValue = numberINPUTS[13].value
+				dieResult = dieResult - parseInt(numberINPUTS[13].value)
+			}
+			resultP.textContent = dieResult
+
 			
 		}
 		else{
@@ -367,19 +351,19 @@ for(let i=0; i<rollBUTTONS.length; i++){
 			rollDie(dieNum)
 			let radioINPUTS = document.querySelectorAll(`.selection input[type=radio]`)
 
-			setTimeout(function(){
-				if(radioINPUTS[14].checked === true && parseInt(numberINPUTS[16].value) > 0){
-					addSub = `+`
-					addSubValue = numberINPUTS[16].value
-					dieResult = dieResult + parseInt(numberINPUTS[16].value)
-				}
-				else if(radioINPUTS[15].checked === true && parseInt(numberINPUTS[16].value) > 0){
-					addSub = `-`
-					addSubValue = numberINPUTS[16].value
-					dieResult = dieResult - parseInt(numberINPUTS[16].value)
-				}
-				resultP.textContent = dieResult
-			}, 350);
+	
+			if(radioINPUTS[14].checked === true && parseInt(numberINPUTS[16].value) > 0){
+				addSub = `+`
+				addSubValue = numberINPUTS[16].value
+				dieResult = dieResult + parseInt(numberINPUTS[16].value)
+			}
+			else if(radioINPUTS[15].checked === true && parseInt(numberINPUTS[16].value) > 0){
+				addSub = `-`
+				addSubValue = numberINPUTS[16].value
+				dieResult = dieResult - parseInt(numberINPUTS[16].value)
+			}
+			resultP.textContent = dieResult
+		
 
 		}
 	})
